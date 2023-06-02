928d5d3635a6df2d510d3e1b27a10f20d53871d0
ZakariyeAden <zakariye5417@gmail.com>
2023-06-02 16:45:22 -0500

    Q1 and Q2

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,25 @@
 console.log('***** Music Collection *****')
+
+
+// Create a variable `collection` that starts as an empty array.
+
+let collection = [];
+
+
+// Question 2 : Add a function named `addToCollection`
+function addToCollection(title,artist,yearPublished){
+  // Object
+  let album = {
+    albumtitle: title,
+    albumtArtist: artist,
+    albumYear: yearPublished
+  }
+  // Push it to the Collection array
+  collection.push(album);
+  return album;
+}
+// Show in the Console
+addToCollection('Music', 'Ahmed', 2000);
+console.log('Collection:', collection);
+
+
