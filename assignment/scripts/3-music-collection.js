582bd68d56a3da5cacf4e6a85989cdfa3bd477d9
ZakariyeAden <zakariye5417@gmail.com>
2023-06-02 18:54:13 -0500

    update

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,7 +8,7 @@ let collection = [];
 
 // Question 2 : Add a function named `addToCollection`
 function addToCollection(title,artist,yearPublished){
-  // Object
+  // Make an Object
   let album = {
     albumtitle: title,
     albumtArtist: artist,
@@ -20,6 +20,37 @@ function addToCollection(title,artist,yearPublished){
 }
 // Show in the Console
 addToCollection('Music', 'Ahmed', 2000);
+addToCollection('Music2', 'abdi', 1992);
 console.log('Collection:', collection);
+
+
+// Question 3 : Add a function named `showCollection`.
+function showCollection(collect){
+  // Loop through Array
+  for(let collects of collect){
+    console.log(`title by ${collects.albumtArtist}, published by ${collects.albumYear}`);
+  }
+}
+// Show in the Console
+showCollection(collection);
+// console.log('showCollection', showCollection());
+
+
+
+// Question 4 : Add a function named `findByArtist`. This function should:
+function findByArtist(artist){
+  let storage = [];
+    for(let singer of collection){
+      if(singer.albumtArtist === artist){
+        storage.push(singer)
+      } else {
+        storage = [];
+      }
+      return storage;
+    }
+  }
+
+console.log('Find My Artist', findByArtist('Ahmed'));
+
 
 
